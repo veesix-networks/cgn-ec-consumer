1a8a48fce3713ff96f2f1b88cb6b280153718aa9
BSpendlove <8687668+BSpendlove@users.noreply.github.com>
2025-04-26 11:44:36 +0000

    Fix ruff formatting

--- cgn_ec_consumer/outputs/__init__.py
+++ cgn_ec_consumer/outputs/__init__.py
@@ -1,4 +1,10 @@
-__all__ = ["HTTPWebhookOutput", "TimeScaleDBOutput", "KafkaOutput", "RedisOutput", "AMQPOutput"]
+__all__ = [
+    "HTTPWebhookOutput",
+    "TimeScaleDBOutput",
+    "KafkaOutput",
+    "RedisOutput",
+    "AMQPOutput",
+]
 
 from cgn_ec_consumer.outputs.http import HTTPWebhookOutput
 from cgn_ec_consumer.outputs.timescaledb import TimeScaleDBOutput
