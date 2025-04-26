38fd920e65ef4026bd5cf964d070eef004dd45b7
BSpendlove <8687668+BSpendlove@users.noreply.github.com>
2025-04-26 11:19:15 +0000

    Initial build out

--- cgn_ec_consumer/outputs/__init__.py
+++ cgn_ec_consumer/outputs/__init__.py
@@ -1,6 +1,7 @@
-__all__ = ["HTTPWebhookOutput", "TimeScaleDBOutput", "KafkaOutput", "RedisOutput"]
+__all__ = ["HTTPWebhookOutput", "TimeScaleDBOutput", "KafkaOutput", "RedisOutput", "AMQPOutput"]
 
 from cgn_ec_consumer.outputs.http import HTTPWebhookOutput
 from cgn_ec_consumer.outputs.timescaledb import TimeScaleDBOutput
 from cgn_ec_consumer.outputs.kafka import KafkaOutput
 from cgn_ec_consumer.outputs.redis import RedisOutput
+from cgn_ec_consumer.outputs.amqp import AMQPOutput
